8931e0fb34939e646b411dbe92e87476b4edb1f0
aidenkang99 <aidentufts22@gmail.com>
2019-11-05 21:19:48 -0500

    Add files via upload

---
+++ setlist.js
@@ -0,0 +1,64 @@
+var arr = [];
+var hasSwitched = false;
+
+$.getJSON( "setlist.json", function( data ) {
+    $.each( data, function(k, v) {
+        var info = [];
+        $.each(v, function(key, value){
+            info.push(value);
+        });
+        var song = new Song(info);
+        arr.push(song);
+    });
+
+});
+
+function Song(info){
+    this.songname = info[0];
+    this.artist = info[1];
+    this.genre = info[2];
+    this.year = info[3];
+}
+
+$(document).ready (
+	function(){
+        display();
+    }
+);
+
+var li = $('<li/>').appendTo('#menu');
+var sub_ul = $('<ul/>').appendTo(li);
+
+function display(){
+    $("#menu li ul").attr('id', 'list');
+    $.each(arr, function (key, value) {
+        var sub_li = $('<li/>').html(arr[key].songname+'<br/>'+"Artist: " + arr[key].artist
+                                     +'<br/>'+"Genre: "+arr[key].genre+'<br/>'+"Released:  "+ arr[key].year);
+        sub_ul.append(sub_li);
+    });   
+}
+
+
+
+function onlyDisplay(){
+    var newLi = $('<li/>');
+    newLi.attr('id', 'list');
+    var sub_ul2 = $('<ul/>').appendTo(newLi);
+
+    $.each(arr, function (key, value) {
+        var thisList = document.getElementById("whichGenre");
+        var thisGenre = thisList.options[thisList.selectedIndex].value;
+        if (arr[key].genre == thisGenre){
+            var sub_li = $('<li/>').html(arr[key].songname+'<br/>'+"Artist: " + arr[key].artist
+                                        +'<br/>'+"Genre: "+arr[key].genre+'<br/>'+"Released:  "+ arr[key].year);
+            sub_ul2.append(sub_li);
+        }
+    });
+    $('#list').replaceWith(newLi);
+    //needs to replace sub_ul2 with a new one
+}
+
+$(document).on('submit', '#dropdown', function(event) {
+    event.preventDefault();
+    onlyDisplay();
+});
